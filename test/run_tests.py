3d54cea42a6eecf8027bdbd47ac193da8fcf8062
Thabo Krick <thabokrick@gmail.com>
2025-01-30 09:31:15 +0100

    comments in python script and actions yml

--- test/run_tests.py
+++ test/run_tests.py
@@ -3,19 +3,23 @@ from datetime import datetime
 
 from btc_embedded import EPRestApi, util
 
-# expect epp file matching the mdl/slx file name the 'model' directory
+#
+# (1) Connect to BTC EmbeddedPlatform API
+#
+ep = EPRestApi()
+
+#
+# (2) Load and update the mil+sil test project
+#
 work_dir = os.path.abspath('test')
 epp_file = os.path.join(work_dir, 'seat_heating_control_converted.epp')
 project_name = os.path.basename(epp_file)[:-7]
-
-# BTC EmbeddedPlatform API object
-ep = EPRestApi()
-
-# Load a BTC EmbeddedPlatform profile (*.epp) and update it
 ep.get(f'profiles/{epp_file}', message="Loading profile")
 ep.put('architectures?performUpdateCheck=true', message="Updating model & generating code")
 
-# Execute requirements-based tests
+#
+# (3) Execute requirements-based tests
+#
 exec_start_time = datetime.now()
 scopes = ep.get('scopes')
 scope_uids = [scope['uid'] for scope in scopes]
@@ -28,21 +32,23 @@ rbt_response = ep.post('scopes/test-execution-rbt', rbt_exec_payload, message="R
 util.print_rbt_results(rbt_response)
 test_cases = ep.get('test-cases-rbt')
 
-# B2B test (with automatic test generation for full MCDC coverage)
+#
+# (4) Generate stimuli for full MCDC coverage and run B2B test
+#
 vector_gen_settings = {
     'scopeUid'  : toplevel_scope_uid,
     'pllString' : 'MCDC' 
 }
 ep.post('coverage-generation', vector_gen_settings, message="Generating vectors for structural coverage")
 b2b_coverage = ep.get(f"scopes/{toplevel_scope_uid}/coverage-results-b2b")
-
 # B2B MIL vs. SIL execution
 b2b_response = ep.post(f"scopes/{toplevel_scope_uid}/b2b", { 'refMode': 'SL MIL', 'compMode': 'SIL' }, message="Running B2B test")
 util.print_b2b_results(b2b_response, b2b_coverage)
 
-# Create project report
+#
+# (5) Create HTML report and JUnit XML report
+#
 report = ep.post(f"scopes/{toplevel_scope_uid}/project-report", message="Creating test report")
-# export project report to a file called 'report.html'
 report_dir = os.path.abspath('reports')
 ep.post(f"reports/{report['uid']}", { 'exportPath': report_dir, 'newName': 'report' })
 # Dump JUnit XML report
@@ -58,5 +64,4 @@ util.dump_testresults_junitxml(
 
 # Save *.epp
 ep.put('profiles', { 'path': epp_file }, message="Saving profile")
-
-print('Finished with workflow and create a test report here: ' + report_dir)
+print('Done')
